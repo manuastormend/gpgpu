bc7975290cdf09961c0878cd253f390a96cea6f4
Manuela <manuastormend@gmail.com>
2026-03-22 16:30:34 -0300

    Manu: Arreglo parte 3 ejercicio 2

--- tarea1/ejercicio2parte3.cpp
+++ tarea1/ejercicio2parte3.cpp
@@ -9,31 +9,37 @@ int main() {
 
     srand((unsigned int)time(NULL));
 
-    // Parametros interactivos
+    // Parametro interactivo
     int N = pedir_n_interactivo();
-    int orden = pedir_orden_interactivo();
  
     float *A = new float[N*N]; //Inicializo matriz A
     float *B = new float[N*N]; //Inicializo matriz B
     float *C = new float[N*N]; //Inicializo matriz C
- 
-    for (int i = 0; i < N*N; i++) //Inicializo matriz C en 0
-        C[i] = 0;
 
     for (int i = 0; i < N*N; i++) {
         A[i] = rand() % 10;
         B[i] = rand() % 10;
     }
 
-    //tomo el tiempo que le lleva multiplicar las matrices
-    clock_t inicio, fin;
-    double tiempo;
-    inicio = clock();
-    matrix_mult_por_orden(A, B, C, N, orden);
-    fin = clock();
-    tiempo = (double)(fin - inicio) / CLOCKS_PER_SEC;
-    std::cout << "Orden utilizado: " << nombre_orden_recorrida(orden) << "\n";
-    imprimirTiempo(tiempo);
+    std::cout << "\nResultados para N = " << N << ":\n";
+    std::cout << "----------------------------------------\n";
+
+    for (int orden = 1; orden <= 6; orden++) {
+        for (int i = 0; i < N * N; i++) // Reinicio C para cada medicion
+            C[i] = 0.0f;
+
+        // tomo el tiempo que le lleva multiplicar las matrices
+        clock_t inicio = clock();
+        matrix_mult_por_orden(A, B, C, N, orden);
+        clock_t fin = clock();
+        double tiempo = (double)(fin - inicio) / CLOCKS_PER_SEC;
+        double mflops = ((double)N * N * N) / (tiempo * 1e6);
+
+        std::cout << "Orden utilizado: " << nombre_orden_recorrida(orden) << "\n";
+        imprimirTiempo(tiempo);
+        std::cout << "Rendimiento: " << mflops << " MFLOPS\n\n";
+    }
+
     delete[] A;
     delete[] B;
     delete[] C;
